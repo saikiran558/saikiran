9c0507950813df2c54538bfc68aa9b67b6afb6a0
HOME <HOME@DESKTOP-N1AL3ED>
2020-08-06 16:34:54 +0530

    commit

---
+++ Actitime/src/test/java/com/actitime/timetrack/Demo.java
@@ -0,0 +1,16 @@
+package com.actitime.timetrack;
+
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Demo  {
+@Test
+public void runTest()
+{
+	Reporter.log("successfully running",true);
+	Reporter.log("successfully running",true);
+	Reporter.log("successfully running",true);
+}
+}
+
